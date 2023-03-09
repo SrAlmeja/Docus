904960bbce6ebda80bd7bb97046da95e406b5b85
Angel Legorreta <cdmx1895@amerike.edu>
2023-03-09 02:09:47 -0600

    cambios configuracion

--- Angel-Lego/docusaurus.config.js
+++ Angel-Lego/docusaurus.config.js
@@ -11,17 +11,19 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Docus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'SrAlmeja', // Usually your GitHub org/user name.
+  projectName: 'Docus', // Usually your repo name.
+  deploymentBranch:"gh-pages", 
+  trailingSlash:false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
